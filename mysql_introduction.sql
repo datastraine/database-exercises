3a60680ababeb87bdd3a539302fb08031987bb3e
Anthony Strainee <anthony.straine@gmail.com>
2020-07-20 14:22:13 -0500

    updated answer 9 for clarity

--- mysql_introduction.sql
+++ mysql_introduction.sql
@@ -14,5 +14,16 @@ select database();
 
 SHOW CREATE DATABASE zillow; 
 
+# Table Exercise 
 
-
+# 3
+use `employees`;
+# 4
+show tables;
+# 5 - VARCHAR, INT, DATE
+# 6 - salaries, 
+# 7 - all of them
+# 8 - titles, salaries, employees, dept manager, dept_emp
+# 9 - Table Departments Column dept_no will join with table dept_emp and then pull emp_no to join to table employees
+#10 
+Show Create Table dept_manager;
