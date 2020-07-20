d2b74ba761193d37d7d613ea731be51314a3a97f
Anthony Strainee <anthony.straine@gmail.com>
2020-07-20 15:52:42 -0500

    adds select exercises

---
+++ select_exercises.sql
@@ -0,0 +1,21 @@
+select * from albums
+where artist = 'Pink Floyd';
+
+select release_date from albums
+where name = "Sgt. Pepper's Lonely Hearts Club Band";
+
+select genre from albums
+where name = "Nevermind";
+
+select * from albums
+where release_date like '199%';
+
+select * from albums
+where sales < 20;
+
+select * from albums
+where genre = "Rock"
+--because SQL is fairly explict with and requires a contain or like function 
+--to pull all sub grenes of rock as they are in the same column
+
+
